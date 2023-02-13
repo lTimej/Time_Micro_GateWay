6d2cd939b7a25c411cc6dd0a622a41f315c139a7
lTimej <lTimej@126.com>
2023-02-13 11:13:03 +0800

    gateway小栗子

--- server/handler/index.go
+++ server/handler/index.go
@@ -11,5 +11,7 @@ type Test struct{}
 
 func (e *Test) UserService(ctx context.Context, in *pb.UserRequest, out *pb.UserResponse) error {
 	fmt.Println("hahah")
+	//给out赋值
+	out.Msg = fmt.Sprintf("%s===%d", "hahaha", in.Id)
 	return nil
 }
--- server/main.go
+++ server/main.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"fmt"
+	"liujun/Time_Micro_GateWay/server/common"
+	"liujun/Time_Micro_GateWay/server/handler"
+
 	consul "github.com/asim/go-micro/plugins/registry/consul/v4"
 	"go-micro.dev/v4"
 	"go-micro.dev/v4/logger"
 	"go-micro.dev/v4/registry"
-	"liujun/Time_Micro_GateWay/server/common"
-	"liujun/Time_Micro_GateWay/server/handler"
 
 	pb "liujun/Time_Micro_GateWay/server/proto"
 	"time"
-)
-
-var (
-	service = "Time_micro_gateway"
-	version = "latest"
 )
 
 func main() {
@@ -30,18 +26,18 @@ func main() {
 	})
 
 	srv := micro.NewService(
-		micro.Name(service),
-		micro.Version(version),
+		micro.Name(common.ServiceName),
+		micro.Version(common.Version),
 		micro.Registry(consul_registry),
-		micro.Address(":8081"),
+		micro.Address(":8081"), //Transport [http] Listening on [::]:8081
 		micro.RegisterTTL(time.Second*30),
 		micro.RegisterInterval(time.Second*30))
 
-	pb.NewUserService(service, srv.Client())
-	//注册服务
+	//注册handler,将Test实例注册到服务，供客户端回调时调用
 	if err := pb.RegisterUserServiceHandler(srv.Server(), new(handler.Test)); err != nil {
 		logger.Fatal(err)
 	}
+
 	// 启动服务
 	if err := srv.Run(); err != nil {
 		logger.Fatal(err)
