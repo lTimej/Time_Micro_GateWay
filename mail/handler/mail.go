2542b67ea607e9c5e97925eb450284ca3a2e8245
lTimej <lTimej@126.com>
2023-02-17 01:01:14 +0800

    feat:添加邮箱服务

--- mail/handler/mail.go
+++ mail/handler/mail.go
@@ -1,1 +1,18 @@
 package handler
+
+import (
+	"context"
+	pb "liujun/Time_Micro_GateWay/mail/proto"
+)
+
+type MailHandler struct {
+}
+
+func (mh *MailHandler) TestMail(ctx context.Context, in *pb.MailTestRequest, out *pb.MailTestResponse) error {
+	return nil
+}
+
+func NewMailHandlerService() *MailHandler {
+	handler := new(MailHandler)
+	return handler
+}
--- mail/main.go
+++ mail/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/asim/go-micro/plugins/registry/consul/v4"
+	"go-micro.dev/v4"
+	"go-micro.dev/v4/logger"
 	"go-micro.dev/v4/registry"
 	"liujun/Time_Micro_GateWay/mail/common"
+	"liujun/Time_Micro_GateWay/mail/handler"
+	pb "liujun/Time_Micro_GateWay/mail/proto"
+	"time"
 )
 
 func main() {
@@ -15,4 +21,18 @@ func main() {
 			fmt.Sprintf("%s:%d", consul_addr, consul_port),
 		}
 	})
+	srv := micro.NewService(
+		micro.Name(common.ServiceName),
+		micro.Version(common.Version),
+		micro.Registry(consul_registry),
+		micro.Address(":8081"), //Transport [http] Listening on [::]:8081
+		micro.RegisterTTL(time.Second*30),
+		micro.RegisterInterval(time.Second*30))
+	if err := pb.RegisterMailServiceHandler(srv.Server(), handler.NewMailHandlerService()); err != nil {
+		logger.Fatal(err)
+	}
+	// 启动服务
+	if err := srv.Run(); err != nil {
+		logger.Fatal(err)
+	}
 }
