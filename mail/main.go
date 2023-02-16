d5340a1e2c81539ca9fa4e3fb8e54df75f9abe4a
lTimej <lTimej@126.com>
2023-02-17 00:50:46 +0800

    feat:添加邮箱服务

--- mail/main.go
+++ mail/main.go
@@ -1,1 +1,18 @@
-package mail
+package main
+
+import (
+	"fmt"
+	"go-micro.dev/v4/registry"
+	"liujun/Time_Micro_GateWay/mail/common"
+)
+
+func main() {
+	config := common.Config
+	consul_addr := config.String("consul_addr")
+	consul_port, _ := config.Int("consul_addr")
+	consul_registry := consul.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			fmt.Sprintf("%s:%d", consul_addr, consul_port),
+		}
+	})
+}
